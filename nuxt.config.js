e41c8f519f6044e64af11290c4930ee799183505
nh487 <nh487@cam.ac.uk>
2018-11-30 21:59:17 +0000

    Admin css fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,9 +12,6 @@ export default {
     },
     generate: {
         routes
-    },
-    css: [
-        '@/assets/css/main.css'
-    ]
+    }
 }
 
--- pages/admin.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"></script>
-</template>
-<script>
-export default {
-  layout: "none"
-};
-</script>
