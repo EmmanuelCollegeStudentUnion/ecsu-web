a26c0820c4ddf0019fef128fae5447e60e50979e
nh487 <nh487@cam.ac.uk>
2018-12-01 23:21:08 +0000

    Reorganised room database

--- pages/welfare/index.vue
+++ pages/welfare/index.vue
@@ -8,7 +8,10 @@
       <li v-for="post in welfare" v-bind:key="post.name">
         <a v-bind:href="post.url" v-bind:title="post.title">{{post.title}}</a>
       </li>
-    </ul>
+    </ul>You can also request a pregnacy kit
+    <nuxt-link to="/welfare/pregnacy_kit">here</nuxt-link>
+    <span>or for a general welfare request</span>
+    <nuxt-link to="/welfare/welfare_request">use this link</nuxt-link>
   </section>
 </template>
 
