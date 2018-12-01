f7bea600a229395963fd8fe4b48ae15098cf1aae
nh487 <nh487@cam.ac.uk>
2018-12-01 19:23:41 +0000

    Added pages for rooms

---
+++ pages/rooms/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <header>
+      <h1>Room database</h1>
+    </header>
+
+    <li v-for="room in rooms" v-bind:key="room.name">
+      <nuxt-link :to="room.url" :title="room.title" v-text="room.title"></nuxt-link>
+    </li>
+  </section>
+</template>
+
+<script>
+import content from "@/content";
+export default {
+  layout: "default",
+  asyncData: async ({ params }) => ({
+    rooms: await content("rooms")
+  })
+};
+</script>
+
+<style>
+</style>
