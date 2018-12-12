55022b17ac49c1bcf31198efccfc0885200e82bf
nh487 <nh487@cam.ac.uk>
2018-12-12 22:17:57 +0000

    Better error page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>An error occurred</h1>
+    <a href="/">Home page</a>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted: function() {
+    //Refresh when an error happens in case its we've lost internet connection.
+    if (!this.$route.query.refresh) {
+      const redirect = this.$route.path + "?refresh=1";
+      window.location.replace(redirect);
+    }
+  }
+};
+</script>
