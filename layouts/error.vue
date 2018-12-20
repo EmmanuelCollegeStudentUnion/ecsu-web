62cab31bf5569272a2467f94c6a7411640fbc493
nh487 <nh487@cam.ac.uk>
2018-12-20 16:25:20 +0000

    Better error message

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1>An error occurred. Try refreshing the page</h1>
+    <h1>Something went wrong. Try refreshing the page</h1>
     <a href="/">Home page</a>
   </div>
 </template>
