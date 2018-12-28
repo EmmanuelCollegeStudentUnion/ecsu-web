8140f2812cea7b3dbe16b3f37f3c8d46a3b07486
nh487 <nh487@cam.ac.uk>
2018-12-28 18:35:15 +0000

    Update compare for events

--- content/index.js
+++ content/index.js
@@ -6,12 +6,21 @@ const collator = new Intl.Collator(undefined, {
 function ordering(x) {
     return x.ordering ? x.ordering : 0
 }
+function pubDate(x) {
+    return x.pubdate ? new Date(x.pubdate) : new Date(0)
+}
+function dateTime(x) {
+    return x.datetime ? new Date(x.datetime) : new Date(0)
+}
 function compare(x, y) {
     if (ordering(y) !== ordering(x)) {
         return ordering(y) - ordering(x)
+    } else if (dateTime(x) !== dateTime(y)) {
+        return +(dateTime(x) < dateTime(y)) - +(dateTime(y) < dateTime(x));
+    } else if (pubDate(x) !== pubDate(y)) {
+        return +(pubDate(x) < pubDate(y)) - +(pubDate(y) < pubDate(x));
     } else {
         return collator.compare(x.title, y.title)
-
     }
 
 }
