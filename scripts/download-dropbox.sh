c8081e827c75ff6e94e739aba4239598674d127d
nh487 <nh487@cam.ac.uk>
2019-01-13 20:12:28 +0000

    Dropbox case fix

--- scripts/download-dropbox.sh
+++ scripts/download-dropbox.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 curl -X POST https://content.dropboxapi.com/2/files/download_zip \
     --header "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
-    --header 'Dropbox-API-Arg: {"path": "/Downloads"}' \
+    --header 'Dropbox-API-Arg: {"path": "/downloads"}' \
     -o downloads.zip
 unzip -o ./downloads -d ./static
 rm downloads.zip -f
