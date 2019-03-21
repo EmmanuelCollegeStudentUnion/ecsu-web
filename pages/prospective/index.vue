e76639f8cbcac8ea6617a3781d966ca34733cb64
nh487 <nh487@cam.ac.uk>
2019-03-21 15:42:49 +0000

    Remove old prospectus

--- pages/prospective/index.vue
+++ pages/prospective/index.vue
@@ -4,29 +4,9 @@
     <template slot="media">
       <ImageCard :image="image"/>
     </template>
-    <p>Find out more about Emmanuel College from its Student Union!</p>
+    <h3>Find out more about Emmanuel College from its Student Union!</h3>
 
-    <p>
-      Want to know more about Emma? Then you're in the right place! If you just want to get an idea of what
-      student
-      life is like at Emmanuel, or 'Emma' as it's fondly known, or you're even thinking of applying, then have a
-      look
-      around.
-      <br>
-      <br>
-    </p>
-
-    <p>
-      The Emmanuel College Alternative Prospectus has been written, edited and produced entirely by undergraduates
-      at
-      Emma, it's here to give you the low-down on life, work and play at Emma from the people who know it best:
-      the
-      students themselves. It aims to tell it as it really is. Click on the link below to access!
-    </p>
-
-    <p>
-      <a href="http://www.emma.cam.ac.uk/publications/Emma_Alt_Prospectus.pdf">Prospectus</a>
-    </p>
+    <p>Want to know more about Emma? Then you're in the right place! If you just want to get an idea of what student life is like at Emmanuel, or 'Emma' as it's fondly known, or you're even thinking of applying, then have a look around.</p>
   </PostPage>
 </template>
 
